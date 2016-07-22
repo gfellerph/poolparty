6a4b103002e9c3709949bfb86c9623cedcfb4d7e
Philipp Gfeller <tuelsch@gmail.com>
2016-07-23 00:18:14 +0200

    Add auth store

---
+++ src/components/auth/auth-store.js
@@ -0,0 +1,20 @@
+import store from 'src/store';
+import router from 'src/router';
+
+export default {
+  state: {
+    loggedIn: false,
+    user: null
+  },
+  mutations: {
+    LOGIN: (state, action) => {
+      state.loggedIn = true;
+      state.user = action.user;
+    },
+
+    LOGOUT: (state, action) => {
+      state.loggedIn = false;
+      state.user = null;
+    }
+  }
+}
