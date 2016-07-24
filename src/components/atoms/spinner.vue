ceec4160f755f4eb35f297223b445f8dc84dfdb0
Philipp Gfeller <tuelsch@gmail.com>
2016-07-24 23:03:10 +0200

    Add spinner

---
+++ src/components/atoms/spinner.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  span.spinner
+</template>
+
+<style lang="scss">
+  .spinner {
+    &:before{
+      content: '.';
+      animation: spin 2s infinite;
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      content: '.';
+    }
+    33% {
+      content: '..';
+    }
+    66% {
+      content: '...';
+    }
+  }
+</style>
