efc8c40f67a2369fd41d07e10c0d4a794a522cf7
Philipp Gfeller <tuelsch@gmail.com>
2016-07-24 23:05:58 +0200

    Add pool store

--- src/config/store.js
+++ src/config/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from 'components/auth/auth-store';
+import pool from 'components/pool/pool-store';
 
 Vue.use(Vuex);
 
@@ -8,5 +9,6 @@ export default new Vuex.Store({
   strict: true,
   modules: {
     auth,
+    pool,
   },
 });
