617bff5f25a30291330f343a29df603d62a82ec4
Philipp Gfeller <tuelsch@gmail.com>
2016-07-24 23:03:26 +0200

    Add auth guard

---
+++ src/components/auth/auth-guard.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    slot(name="auth" v-if="user")
+    slot(name="no-auth" v-if="!user")
+      p Not authenticated
+</template>
+
+<script>
+  export default {
+    vuex: {
+      getters: {
+        user: state => state.auth.user,
+      },
+    },
+  };
+</script>
