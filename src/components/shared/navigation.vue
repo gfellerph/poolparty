2d84853408ac00b87878c202383efc40749986b5
Philipp Gfeller <tuelsch@gmail.com>
2016-07-24 23:05:40 +0200

    Simple navigation

---
+++ src/components/shared/navigation.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  nav
+    ul
+      li
+        a(v-link="{path: '/', exact: true}") Home
+      li
+        a(v-link="{path: '/pool'}") Resource Pool
+</template>
+
+<style lang="scss">
+  .active {
+    font-weight: bold;
+  }
+</style>
