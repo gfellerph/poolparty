eb73812bfcdc9137f5989fb1d80a1e5615a43647
Philipp Gfeller <tuelsch@gmail.com>
2016-07-24 23:04:32 +0200

    Add filter component for resources

---
+++ src/components/pool/resource-filter.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  div
+    input(
+      v-model="cFilter"
+      debounce="200"
+    )
+</template>
+
+<script>
+  export default {
+    computed: {
+      cFilter: {
+        get() {
+          return this.filter;
+        },
+
+        set(value) {
+          this.setFilter(value);
+        },
+      },
+    },
+
+    vuex: {
+      getters: {
+        filter: state => state.pool.filter,
+      },
+      actions: {
+        setFilter({ dispatch }, value) {
+          // Don't dispatch if filter is the same
+          if (value === this.filter) return;
+
+          // Dispatch with new filter object
+          dispatch('SET_FILTER', {
+            filter: value,
+          });
+        },
+      },
+    },
+  };
+</script>
