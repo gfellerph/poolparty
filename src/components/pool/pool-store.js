9e454d150aa2190e085f5ee545a51cdd59795fb0
Philipp Gfeller <tuelsch@gmail.com>
2016-08-01 18:29:11 +0200

    Add skill store

--- src/components/pool/pool-store.js
+++ src/components/pool/pool-store.js
@@ -1,23 +1,48 @@
-import resourceIndex from 'components/pool/resource-index';
-
 export default {
   state: {
     resources: [],
+    skills: [],
     filter: '',
   },
   mutations: {
     ADD_RESOURCE: (state, action) => {
       state.resources.push(action.resource);
-      resourceIndex.add(action.resource);
     },
+
+    /* eslint-disable arrow-body-style */
+    UPDATE_RESOURCE: (state, action) => {
+      state.resources = state.resources.map(resource => {
+        return (resource.key === action.resource.key) ? action.resource : resource;
+      });
+    },
+    /* eslint-enable arrow-body-style */
 
     REMOVE_RESOURCE: (state, action) => {
       state.resources = state.resources.filter(resource => resource.key !== action.resource.key);
-      resourceIndex.remove(action.resource);
+    },
+
+    ADD_SKILL: (state, action) => {
+      state.skills.push(action.skill);
+    },
+
+    /* eslint-disable arrow-body-style */
+    UPDATE_SKILL: (state, action) => {
+      state.skills = state.skills.map(skill => {
+        return (skill.key === action.skill.key) ? action.skill : skill;
+      });
+    },
+    /* eslint-enable arrow-body-style */
+
+    REMOVE_SKILL: (state, action) => {
+      state.skills = state.skills.filter(skill => skill.key !== action.skill.key);
     },
 
     SET_FILTER: (state, action) => {
       state.filter = action.filter;
+    },
+
+    CLEAR_RESOURCES: (state) => {
+      state.resources = [];
     },
   },
 };
