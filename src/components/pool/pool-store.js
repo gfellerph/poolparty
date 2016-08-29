b828dc6031666758c9a06b16d535b56cc0e2de6c
Philipp Gfeller <tuelsch@gmail.com>
2016-08-30 00:05:33 +0200

    Add toggle skill mutation

--- src/components/pool/pool-store.js
+++ src/components/pool/pool-store.js
@@ -12,29 +12,45 @@ export default {
     /* eslint-disable arrow-body-style */
     UPDATE_RESOURCE: (state, action) => {
       state.resources = state.resources.map(resource => {
-        return (resource.key === action.resource.key) ? action.resource : resource;
+        return (resource.id === action.resource.id) ? action.resource : resource;
       });
     },
     /* eslint-enable arrow-body-style */
 
     REMOVE_RESOURCE: (state, action) => {
-      state.resources = state.resources.filter(resource => resource.key !== action.resource.key);
+      state.resources = state.resources.filter(resource => resource.id !== action.resource.id);
     },
 
     ADD_SKILL: (state, action) => {
       state.skills.push(action.skill);
     },
 
+    TOGGLE_SKILL: (state, action) => {
+      state.resources = state.resources.map(resource => {
+        if (resource.id === action.resourceId) {
+          if (resource.skills.indexOf(action.skill.id)) {
+            // Remove
+            resource.skills = resource.skills.filter(skill => skill.id !== action.skill.id);
+          } else {
+            // Add
+            resource.skills = resource.skills.concat([action.skill.id]);
+          }
+        }
+
+        return resource;
+      });
+    },
+
     /* eslint-disable arrow-body-style */
     UPDATE_SKILL: (state, action) => {
       state.skills = state.skills.map(skill => {
-        return (skill.key === action.skill.key) ? action.skill : skill;
+        return (skill.id === action.skill.id) ? action.skill : skill;
       });
     },
     /* eslint-enable arrow-body-style */
 
     REMOVE_SKILL: (state, action) => {
-      state.skills = state.skills.filter(skill => skill.key !== action.skill.key);
+      state.skills = state.skills.filter(skill => skill.id !== action.skill.id);
     },
 
     SET_FILTER: (state, action) => {
@@ -43,6 +59,10 @@ export default {
 
     CLEAR_RESOURCES: (state) => {
       state.resources = [];
+    },
+
+    CLEAR_SKILLS: (state) => {
+      state.skills = [];
     },
   },
 };
