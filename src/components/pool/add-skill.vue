251b5ec2289d6f070df4f0f149b7db79efe8d030
Philipp Gfeller <tuelsch@gmail.com>
2016-08-30 00:05:41 +0200

    Refactor add skill form

---
+++ src/components/pool/add-skill.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  form
+    h3 NEW SKILL
+    p
+      label Add skill
+      input(
+        v-model="skill.name"
+      )
+      button(
+        @click.prevent="saveSkill"
+      ) Add skill
+</template>
+
+<script>
+  import Skill from 'models/skill';
+
+  export default {
+    data() {
+      return {
+        skill: new Skill(),
+      };
+    },
+
+    methods: {
+      saveSkill() {
+        this.skill.set()
+          .catch(err => { throw err; });
+
+        this.skill = new Skill();
+      },
+    },
+  };
+</script>
